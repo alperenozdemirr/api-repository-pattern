e82178a040d3ea3a6dff5b0b8feb9fb27c721df0
Alp Eren Özdemir <ozdemiiralperen@gmail.com>
2023-11-18 23:49:09 +0300

    categories triger add methods

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,11 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Category extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function($category) {
+            $category->deleteChildrenRecursively($category);
+        });
+    }
+
+    protected function deleteChildrenRecursively($category)
+    {
+        $children = $category->children;
+
+        foreach ($children as $child) {
+            $this->deleteChildrenRecursively($child);
+            $child->delete();
+        }
+    }
 
     //Subcategories for each category
     public function children()
